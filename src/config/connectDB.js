dbddedc9ffdf38f3948c7982a683faeb9f5665cc
Quan <phamdinhquan202@gmail.com>
2024-02-03 23:11:40 +0700

    change file

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,12 +1,18 @@
 const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('quanpham', 'quanpham', "APT9BSpF1cKIcaOUTsK2SzaVvYyswlfD", {
-    host: 'localhost',
-    dialect: 'postgres',
-    logging: false
+const sequelize = new Sequelize('sql6681762', 'sql6681762', "hCNMjtkwQC", {
+    host: 'sql6.freemysqlhosting.net',
+    dialect: 'mysql',
+    logging: true
 });
+// Name: sql6681762
+// Username: sql6681762
+// Password: hCNMjtkwQC
+// Port number: 3306
+// const sequelize = new Sequelize("postgres://quanpham:APT9BSpF1cKIcaOUTsK2SzaVvYyswlfD@dpg-cmv4bcqcn0vc73amic9g-a.singapore-postgres.render.com/quanpham");
 
+// postgres://quanpham:APT9BSpF1cKIcaOUTsK2SzaVvYyswlfD@dpg-cmv4bcqcn0vc73amic9g-a.singapore-postgres.render.com/quanpham
 let connectDB = async()=>{
     try {
         await sequelize.authenticate();
